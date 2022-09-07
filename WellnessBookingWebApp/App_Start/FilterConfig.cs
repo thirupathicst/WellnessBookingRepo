eb9023bd804d96a4fcd24014455b2259197563b7
thirupathi <athirupathi@delaplex.com>
2022-09-07 13:48:00 +0530

    Add project files.

---
+++ WellnessBookingWebApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WellnessBookingWebApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
